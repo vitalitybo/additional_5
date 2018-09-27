fdeda9096c7b880d564523c575aae0fd8e8c9b01
vitalitybo <v2.borisevich@gmail.com>
2018-09-27 22:26:19 +0300

    Additional_5 All tests passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const arr = str.split('');
+
+  for (let i = 0; i < arr.length; i++) {
+
+    for (let j = 0, brLen = bracketsConfig.length; j < brLen; j++) {
+      const conditionOne = arr[i] === bracketsConfig[j][0];
+      const conditionTwo = arr[i + 1] === bracketsConfig[j][1];
+
+      if (conditionOne && conditionTwo) {
+        arr.splice(i, 1);
+        arr.splice(i, 1);
+        i = i - 2;
+      }
+    }
+  }
+
+  return !arr.length;
+};
